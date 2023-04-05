a07d9f60f104e36ad9a723b7666ed40d1ceed458
Bismark Mesquita <bismark.nascimento@unimedfortaleza.com.br>
2023-04-05 10:17:06 -0300

    primeiro commit

---
+++ js/conectaApi.js
@@ -0,0 +1,33 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos")
+    const conexaoConvertida = await conexao.json()
+
+    return conexaoConvertida
+}
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type:": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
+export const conectaApi = {
+    listaVideos,
+    criaVideo
+}
+
+
+
+// usar essa aplicação para colocar uma funcionalidade parecida no projeto "Geek Burguer", com um formulário onde a pessoa possa adicionar novos itens no cardápio direto da aplicação. Mas essa funcionalidade precisa ser exclusiva do estabelecimento.
